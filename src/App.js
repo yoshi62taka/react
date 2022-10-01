adc3cf39c1dc1fa318fbc8a9d254b3fece06d106
yoshi62taka <yoshitaka.yagawa@gmail.com>
2022-10-02 02:20:43 +0900

    遊び

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          初めての変更
         </a>
       </header>
     </div>
---
+++ src/Train.js
@@ -0,0 +1,19 @@
+
+function Train() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          これが最初
+        </p>
+        <a
+          href="https://reactjs.org"
+        >
+          ちょっと考えてみて
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Train;
